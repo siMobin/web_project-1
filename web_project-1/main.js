1bbe79d95fa57fc46ce988dd83e65fc4ca7ce236
siMobin <shakibulislammobin@gmail.com>
2022-11-28 18:22:23 +0600

    upload

--- web_project-1/main.js
+++ web_project-1/main.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    pTag = document.querySelector("div");
-    newVal = document.createElement("p");
-    newVal.innerHTML = ' ';
-    pTag.appendChild(newVal);
-  }); 
-
-var pos1 = window.pageYOffset;
-window.onscroll = function() {
-var pos2 = window.pageYOffset;
-  if (pos1 > pos2) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-50px";
-  }
-  pos1 = pos2;
-}
+//
